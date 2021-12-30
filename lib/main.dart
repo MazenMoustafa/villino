2f97f7778d9a89fb73dd07510d96a1dbba3646d8
Mumen Ahmed <mumenahmed1811@gmail.com>
2021-12-30 18:21:59 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:vallino/util/size_config.dart';
+import 'package:vallino/view/screens/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Main(),
+      ),
+    );
+  }
+}
+
+
+
+class Main extends StatefulWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig.init(context);
+    return SplashScreen();
+  }
+}
+
+
---
+++ lib/util/color_resources.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ColorResources {
+
+  static const Color PRIMARY_COLOR = Color.fromRGBO(166, 28, 102, 1.0);
+  static const Color WHITE_PRIMARY_COLOR = Color(0xffE7D3DF);
+  static const Color BG_COLOR = Color.fromRGBO(246, 238, 238, 1);
+
+  static const Color HINT_TEXT_COLOR = Color(0xff9E9E9E);
+  static const Color LIGHT_GREY = Color(0xffF4F5F7);
+  static const Color DARK_GREY = Color(0xff707070);
+  static const Color WHITE_GREY = Color(0xffF4F5F6);
+
+  static const Color DRAWER_ELEMENT_1 = Color(0xffFBDEEE);
+  static const Color DRAWER_ELEMENT_2 = Color(0xffD0E9FA);
+  static const Color DRAWER_ELEMENT_3 = Color(0xffD2F6EB);
+
+  static const Color SHADOW = Color(0xffDCDCDC);
+
+
+
+  static const Color BLACK = Color(0xff000000);
+  static const Color WHITE = Color(0xffFFFFFF);
+
+  static const Color COLOR_BLUE = Color(0xff00ADE3);
+  static const Color COLUMBIA_BLUE = Color(0xff00ADE3);
+  static const Color LIGHT_SKY_BLUE = Color(0xff8DBFF6);
+  static const Color HARLEQUIN = Color(0xff3FCC01);
+  static const Color CERISE = Color(0xffE2206B);
+  static const Color GREY = Color(0xffF1F1F1);
+  static const Color RED = Color(0xFFD32F2F);
+  static const Color YELLOW = Color(0xFFFFAA47);
+
+  static const Color GAINS_BORO = Color(0xffE6E6E6);
+  static const Color TEXT_BG = Color(0xFFF4F7FC);
+  static const Color ICON_BG = Color(0xffF9F9F9);
+  static const Color HOME_BG = Color(0xffF0F0F0);
+  static const Color IMAGE_BG = Color(0xffE2F0FF);
+  static const Color SELLER_TXT = Color(0xff92C6FF);
+  static const Color CHAT_ICON_COLOR = Color(0xffD4D4D4);
+  static const Color LOW_GREEN = Color(0xffEFF6FE);
+  static const Color GREEN = Color(0xff23CB60);
+  static const Color FLOATING_BTN = Color(0xff7DB6F5);
+  static const Color COLOR_LIGHT_BLACK = Color(0xFF4A4B4D);
+}
