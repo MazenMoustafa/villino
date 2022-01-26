50f9c74510d32020cd04eb693836ceba87eb2728
Mumen Ahmed <mumenahmed1811@gmail.com>
2022-01-26 17:47:48 +0200

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vallino/util/size_config.dart';
 import 'package:vallino/view/screens/about_us_screen.dart';
 import 'package:vallino/view/screens/guest_form_screen.dart';
@@ -10,9 +14,64 @@ import 'package:vallino/view/screens/splash_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 
-import 'view/screens/select_user_screen.dart';
-
 void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // One Signal
+  //Remove this method to stop OneSignal Debugging
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+  OneSignal.shared.setAppId("9d93a153-9072-4ce0-b6f6-7e4e9b7aaac7");
+
+// The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+    print("Accepted permission: $accepted");
+    log("setNotificationWillShowInForegroundHandler");
+  });
+
+  OneSignal.shared.setNotificationWillShowInForegroundHandler((OSNotificationReceivedEvent event) {
+    // Will be called whenever a notification is received in foreground
+    // Display Notification, pass null param for not displaying the notification
+    event.complete(event.notification);
+    log("setNotificationWillShowInForegroundHandler");
+  });
+
+
+
+  OneSignal.shared.setPermissionObserver((OSPermissionStateChanges changes) {
+    // Will be called whenever the permission changes
+    // (ie. user taps Allow on the permission prompt in iOS)
+    log("setSubscriptionObserver");
+  });
+
+  OneSignal.shared.setSubscriptionObserver((OSSubscriptionStateChanges changes) {
+    // Will be called whenever the subscription changes
+    // (ie. user gets registered with OneSignal and gets a user ID)
+    String? onesignalUserId = changes.to.userId;
+    //log("setSubscriptionObserver");
+    SharedPreferences.getInstance().then((value){
+      value.setString("device_token",onesignalUserId!);
+    });
+  });
+
+  OneSignal.shared.setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+    _MyAppState.handleClickNotification(result);
+    log("Opened");
+  });
+  OneSignal.shared.setEmailSubscriptionObserver((OSEmailSubscriptionStateChanges emailChanges) {
+    // Will be called whenever then user's email subscription changes
+    // (ie. OneSignal.setEmail(email) is called and the user gets registered
+  });
+  OneSignal.shared.getDeviceState().then((deviceState) {
+    print("DeviceState: ${deviceState?.jsonRepresentation()}");
+    SharedPreferences.getInstance().then((value){
+      value.setString("device_token",deviceState!.userId!);
+      log("Token " + deviceState.userId!);
+    });
+    //log(deviceState!.pushToken!);
+  });
+
   runApp(const MyApp());
 }
 
@@ -25,9 +84,11 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  static final navKey = new GlobalKey<NavigatorState>();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navKey,
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       localizationsDelegates: [
@@ -49,6 +110,27 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
+
+  static void handleClickNotification(OSNotificationOpenedResult result) {
+    log( result.notification.additionalData!.toString());
+    navKey.currentState?.push(MaterialPageRoute(
+        builder: (_) => NotificationsScreen()
+    ));
+
+
+    //SharedPreferences.getInstance().then((prefs){
+    // });
+
+      // if(result.notification.additionalData?.containsKey('ad_id') == true){
+      //
+      // }
+      // else{
+      //
+      // }
+
+
+
+  }
 }
 
 
@@ -64,7 +146,7 @@ class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
     SizeConfig.init(context);
-    return NotificationsScreen();
+    return SplashScreen();
   }
 }
 
